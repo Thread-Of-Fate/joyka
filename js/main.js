9c52faea328a7dd046b624d5a49efde2c7c1c6be
Thread-Of-Fate <60323077+Thread-Of-Fate@users.noreply.github.com>
2020-05-20 12:29:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(function(){
+ $('.about-slider').slick({
+ 	infinite: true,
+ 	dots: true,
+ 	arrows: true,
+ 	centerMode: true,
+ 	centerPadding: '140px',
+	slidesToShow: 1,
+	slidesToScroll: 1,
+ 	prevArrow: '<img class="slider-arrows arrow-left" src="img/arrow-left.svg" alt=""></img>',
+  nextArrow: '<img class="slider-arrows arrow-right" src="img/arrow-right.svg" alt=""></img>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        arrows: false,
+        centerPadding: '20px',
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }
+  ]
+ });
+ $('.reviews-slider').slick({
+ 	infinite: true,
+ 	dots: true,
+ 	arrows: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+ 	prevArrow: '<img class="slider-arrows arrow-left" src="img/arrow-left.svg" alt=""></img>',
+  nextArrow: '<img class="slider-arrows arrow-right" src="img/arrow-right.svg" alt=""></img>',
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+        arrows: false,
+        centerPadding: '0px',
+      }
+    }
+  ]
+ });
+ 
+ $('#create-1').click(function(){
+ 	$('#create-1').toggleClass('active');
+ });
+ $('#create-2').click(function(){
+ 	$('#create-2').toggleClass('active');
+ });
+ $('#create-3').click(function(){
+ 	$('#create-3').toggleClass('active');
+ });
+});
